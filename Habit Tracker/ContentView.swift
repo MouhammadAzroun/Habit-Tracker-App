b01a1e1ebcd272f32c5e789c6f6124505ff91cc1
mouhammadazroun <yhma22.azromo@folkuniversitetet.nu>
2023-04-23 20:23:42 +0200

    Initial Commit

---
+++ Habit Tracker/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Habit Tracker
+//
+//  Created by Mouhammad Azroun on 2023-04-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Habit Tracker/Habit_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habit_TrackerApp.swift
+//  Habit Tracker
+//
+//  Created by Mouhammad Azroun on 2023-04-23.
+//
+
+import SwiftUI
+
+@main
+struct Habit_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
